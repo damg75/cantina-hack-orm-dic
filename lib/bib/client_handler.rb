7a7cc6cd013850848a84a9c2d1d6087af2c3f320
Daniel Moran <damg75cd@gmail.com>
2019-12-22 14:56:36 -0400

    Creado un menu, conecta con la db

---
+++ lib/bib/client_handler.rb
@@ -0,0 +1,55 @@
+require_relative 'draw'
+require_relative '../../models/client'
+
+# puts 'Gestion de Clientes:'
+# puts 
+# puts 'q: Lista de clientes/Busqueda de Clientes'
+# puts
+# puts 'w: Crear/Actualizar un Cliente'
+# puts
+# puts 'e: Eliminar un Cliente'
+# puts
+# puts 'x: Salir'
+# puts
+
+class Client_handler
+
+    def self.handler   
+        loop do
+            Draw.main_menu
+            Draw.client_handler_menu
+            puts 'estoy en el client handler'
+            Client_handler.input_handler
+        end
+    end
+    
+    def self.input_handler
+        selector = gets.chomp
+        case selector
+            when 'q'
+                puts
+                puts 'clients list:'
+                puts
+                array = []
+                Client.all.each do |i|
+                    array << i
+                end
+                array.each do |i|
+                    puts
+                    i.attributes.each do |key, value|
+                    print "#{key}: #{value} "
+                    end
+                    puts
+                end
+                puts
+                puts 'Presione cualquier tecla para continuar'
+                continuator = gets.chomp
+            when 'x'
+                exit_client_handler
+        end
+    end
+
+    def exit_client_handler
+        raise StopIteration
+    end
+end
