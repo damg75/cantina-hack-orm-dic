60d479f2dd871666ce9044563a52907177a9eb59
Daniel Moran <damg75cd@gmail.com>
2019-12-30 03:52:53 -0400

    seguimos avanzando

--- lib/bib/shopping_handler.rb
+++ lib/bib/shopping_handler.rb
@@ -124,10 +124,10 @@ class Shopping_handler
                 puts
             end
             puts
-            puts
             if shopping_cart.size > 0
                 shopping_cart.each do |sale|
                     producto = Product.find_by(id: sale.product_id)
+                    puts
                     puts producto.product_name
                     puts
                     sale.attributes.each do |key, value|
@@ -137,25 +137,144 @@ class Shopping_handler
                 end
             end
             puts
-            puts 'Desea seguir agregando productos al carrito? y/n'
+            if shopping_cart.size == 0
+                puts 'Desea proceder a agregar productos a su carrito? y/n'
+            else
+                puts 'Desea seguir agregando productos al carrito? y/n'
+            end
             selector = gets.chomp.to_s
             while selector != 'y' && selector != 'n'
                 puts  'y/n para responder'
                 selector = gets.chomp.to_s
             end
             case selector
-            when 'y'
-                Shopping_handler.cart_shopping(client_shopper,shopping_cart,invoice)
-            when 'n'
-                #deberia confirmar la compra o generar un recibo
-                raise StopIteration
-            end
+                when 'y'
+                    # Shopping_handler.cart_shopping(client_shopper,shopping_cart,invoice)
+                    system('clear')
+                    puts 'Shopping por categoria'
+                    puts
+                    puts 'Lista de Categorias:'
+                    puts
+                    array = []
+                    Category.all.each do |element|
+                        array << element
+                    end
+                    array.each do |element|
+                        puts
+                        element.attributes.each do |key, value|
+                            print "#{key}: #{value} "
+                        end
+                        puts
+                    end
+                    puts
+                    puts 'Escoja la id de la categoria del producto'
+                    cat_id = gets.chomp.to_i
+                    while cat_id != 1 && cat_id != 2 && cat_id != 3
+                        puts 'Ingrese id valido'
+                        cat_id = gets.chomp.to_i
+                    end
+                    category = Category.find_by(id: cat_id)
+                    system('clear')
+                    category.products.each do |products|
+                        puts
+                        products.attributes.each do |key,value|
+                            print "#{key}: #{value} "
+                            puts
+                        end
+                    end
+                    puts
+                    puts 'Escoja id de producto a agregar'
+                    product_id = gets.chomp.to_i
+                    product = Product.find_by(id: product_id)
+                    system('clear')
+                    product.attributes.each do |key,value|
+                        print "#{key}: #{value} "
+                        puts
+                    end
+                    puts
+                    puts "Cantidad diponible de producto: #{product.product_quantity}"
+                    puts
+                    puts 'Indique cantidad de producto a comprar'
+                    sale_quantity = gets.chomp.to_i
+                    while sale_quantity > product.product_quantity
+                        puts 'Cantidad de compra debe ser menor o igual a la cantidad disponible de producto'
+                        sale_quantity = gets.chomp.to_i
+                    end
+                    puts
+                    sale_price = sale_quantity * product.selling_price
+                    sale_hash = {product_id: product.id, sale_quantity: sale_quantity, sale_price: sale_price, invoice_id: invoice.id}
+                    sale = Sale.new(sale_hash)
+                    puts "Total a pagar por esta adicion al carrito: #{sale_price}"
+                    puts "Total de unidades de producto a agregar al carrito: #{sale_quantity}"
+                    puts 
+                    puts sale.attributes.each do |key,value|
+                        print "#{key}: #{value} "
+                        puts
+                    end
+                    puts 'Para confirmar adicion al carrito y/n'
+                    selector = gets.chomp.to_s
+                    while selector != 'y' && selector != 'n' && selector != 'x'
+                        puts  'y/n para responder'
+                        selector = gets.chomp.to_s
+                    end
+                    case selector
+                        when 'y'
+                            if client_shopper.client_balance >= sale_price
+                                sale.save
+                                product_quantity_new = product.product_quantity - sale_quantity
+                                product_hash = {product_quantity: product_quantity_new}
+                                Product.update(product_id, product_hash)
+                                client_balance_new = client_shopper.client_balance - sale_price
+                                client_hash = {client_balance: client_balance_new}
+                                Client.update(client_shopper.id,client_hash)
+                                client_shopper = Client.find_by(id: client_shopper.id)
+                                shopping_cart << sale
+                                # system('clear')
+                            else   
+                                puts 'Saldo insuficiente'
+                            end
+                        when 'n'
+                            sale.destroy
+                    end
 
-            
-            
-            
-            
-            
+                    continuator = gets.chomp
+                when 'n'
+                    system ('clear')
+                    if shopping_cart.size > 0
+                        puts 'Resumen de su compra:'
+                        puts
+                        client_shopper.attributes.each do |key,value|
+                            print "#{key}: #{value} "
+                            puts
+                        end  
+                        puts
+                        puts
+                        invoice.attributes.each do |key, value|
+                            print "#{key}: #{value} "
+                            puts
+                        end
+                        puts
+                        if shopping_cart.size > 0
+                            shopping_cart.each do |sale|
+                                producto = Product.find_by(id: sale.product_id)
+                                puts
+                                puts producto.product_name
+                                puts
+                                sale.attributes.each do |key, value|
+                                    print "#{key}: #{value} "
+                                    puts
+                                end
+                            end
+                        end
+                    else
+                        puts 'No agrego nada a su carrito de compra'
+                        invoice.destroy
+                    end
+                    puts 
+                    puts 'Presione cualquier tecla para continuar'
+                    continuator = gets.chomp
+                    selector = 'x'
+            end
             break if selector == 'x'
         end
     end
@@ -230,13 +349,23 @@ class Shopping_handler
         end
         case selector
             when 'y'
-                sale.save
-                shopping_cart << sale
+                if client_shopper.client_balance >= sale_price
+                    sale.save
+                    product_quantity_new = product.product_quantity - sale_quantity
+                    product_hash = {product_quantity: product_quantity_new}
+                    Product.update(product_id, product_hash)
+                    client_balance_new = client_shopper.client_balance - sale_price
+                    client_hash = {client_balance: client_balance_new}
+                    Client.update(client_shopper.id,client_hash)
+                    client_shopper = Client.find_by(id: client_shopper.id)
+                    shopping_cart << sale
+                else   
+                    puts 'Saldo insuficiente'
+                end
             when 'n'
                 sale.destroy
         end
 
         continuator = gets.chomp
     end
-
 end    
